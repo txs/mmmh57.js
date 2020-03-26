069ebc0c1b2472a37bb0b36816e7bbfd5491a12b
Andy tseng <txshon@Andys-MacBook-Pro.local>
2020-03-26 10:12:34 +0800

    上課中改動 server

--- nodeExample/adServer.js
+++ nodeExample/adServer.js
@@ -1,21 +1,23 @@
 
 var http=require('http');
 var server=http.createServer(function(req,res){
-        if(req.url=='/'){
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write('<html><body>This is Home Page.</body></html>');
-            res.end();
-        }else if(req.url=='/student'){
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write('<html><body>This is student Page.</body></html>');
-            res.end();
-        }else if(req.url=='/admin'){
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write('<html><body>This is admin Page.</body></html>');
-            res.end();
-        }else
-            res.end('Invalid Request!');
- 
+    console.log(req.url);
+    
+    if(req.url=='/'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write('<html><body>This is Home Page.</body></html>');
+        res.end();
+    }else if(req.url=='/student'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write('<html><body>This is student Page.</body></html>');
+        res.end();
+    }else if(req.url=='/admin'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write('<html><body>This is admin Page.</body></html>');
+        res.end();
+    }else
+        res.end('Invalid Request!');
+
 });
  
 server.listen(3000);
